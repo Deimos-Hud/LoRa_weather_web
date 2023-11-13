a3f3098ed6271a320bcc5fcf0a59f08cd3582977
Deimos-Hud <146753317+Deimos-Hud@users.noreply.github.com>
2023-11-13 09:51:52 -0500

    Update Nov_11a.ino

--- arduino_code/Nov_11a.ino
+++ arduino_code/Nov_11a.ino
@@ -1,93 +1,90 @@
-// Arduino9x_RX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (receiver)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Arduino9x_TX
-
 #include <SPI.h>
 #include <RH_RF95.h>
 
+// LoRa Pins
 #define RFM95_CS 10
-#define RFM95_RST 5 // Use the pin 5 you specified for reset
+#define RFM95_RST 5
 #define RFM95_INT 2
 
-// Change to 434.0 or other frequency, must match RX's freq!
+// LoRa Frequency
 #define RF95_FREQ 915.0
+
+// Size of the userMessage buffer
+#define USER_MESSAGE_SIZE 242
 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-// Blinky on receipt
-#define LED 13
+// Buffer for storing user input
+char userMessage[USER_MESSAGE_SIZE];
 
-void setup() 
-{
-  pinMode(LED, OUTPUT);     
-  pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
+void setup() {
+    pinMode(RFM95_RST, OUTPUT);
+    digitalWrite(RFM95_RST, HIGH);
 
-  while (!Serial);
-  Serial.begin(9600);
-  delay(100);
+    while (!Serial);
+    Serial.begin(9600);
+    delay(100);
 
-  Serial.println("Arduino LoRa RX Test!");
-  
-  // manual reset
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
+    // manual reset
+    digitalWrite(RFM95_RST, LOW);
+    delay(10);
+    digitalWrite(RFM95_RST, HIGH);
+    delay(10);
 
-  while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
-    while (1);
-  }
-  Serial.println("LoRa radio init OK!");
+    while (!rf95.init()) {
+        Serial.println("LoRa radio init failed");
+        while (1);
+    }
+    Serial.println("LoRa radio init OK!");
 
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
-    while (1);
-  }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+    if (!rf95.setFrequency(RF95_FREQ)) {
+        Serial.println("setFrequency failed");
+        while (1);
+    }
 
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
-  // you can set transmitter powers from 5 to 23 dBm:
-  rf95.setTxPower(23, false);
+    rf95.setTxPower(23, false);
 }
 
-void loop()
-{
-  if (rf95.available())
-  {
-    // Should be a message for us now   
-    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    uint8_t len = sizeof(buf);
-    
-    if (rf95.recv(buf, &len))
-    {
-      digitalWrite(LED, HIGH);
-      RH_RF95::printBuffer("Received: ", buf, len);
-      Serial.print("Got: ");
-      Serial.println((char*)buf);
-       Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
-      
-      // Send a reply
-      uint8_t data[] = "all set!";
-      rf95.send(data, sizeof(data));
-      rf95.waitPacketSent();
-      Serial.println("Sent a reply");
-      digitalWrite(LED, LOW);
+void loop() {
+    readUserInput(); // Read user input into userMessage
+
+    if (strlen(userMessage) > 0) {
+
+        rf95.send((uint8_t *)userMessage, strlen(userMessage) + 1);
+        rf95.waitPacketSent();
+
+        memset(userMessage, 0, USER_MESSAGE_SIZE); // Clear the userMessage for next use
+
+        uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+        uint8_t len = sizeof(buf);
+
+        if (rf95.waitAvailableTimeout(3000)) { // Wait for a reply
+            if (rf95.recv(buf, &len)) {
+                Serial.print("Got reply: ");
+                Serial.println((char*)buf);
+                Serial.print("RSSI: ");
+                Serial.println(rf95.lastRssi(), DEC);
+            } else {
+                Serial.println("Receive failed");
+            }
+        } else {
+            Serial.println("No reply, is there a listener around?");
+        }
     }
-    else
-    {
-      Serial.println("Receive failed");
+}
+
+void readUserInput() {
+    int index = 0;
+    while (true) {
+        if (Serial.available()) {
+            char c = Serial.read();
+            if (c == '\n' || index >= (USER_MESSAGE_SIZE - 1)) {
+                break;
+            }
+            userMessage[index] = c;
+            index++;
+        }
     }
-  }
+    userMessage[index] = '\0'; // Null-terminate the string
 }
